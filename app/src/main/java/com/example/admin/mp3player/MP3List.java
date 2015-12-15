0eed8c993342192491950d2387799ba4ce2f8cc1
Ranjit Ghodke <rghodke@ucsd.edu>
2015-12-14 21:44:01 -0800

    User can specify which file to upload and retrieve results
    The user can now upload any file and have it stored on the Parse server
    as well as get a list of all of the files on the server. There is also a
    Progress Dialog implemented for the uploading process.

--- app/src/main/java/com/example/admin/mp3player/MP3List.java
+++ app/src/main/java/com/example/admin/mp3player/MP3List.java
@@ -39,132 +39,22 @@ public class MP3List extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.musicList);
         uploadButton = (Button)findViewById(R.id.uploadButton);
         getListFromParse();
-        uploadData();
-
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("GameScore");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null) {
-                    for (ParseObject x : objects) {
-                        if(x.get("applicantResumeFile") != null){
-
-
-                            try {
-                                System.out.println(new String(x.getBytes("applicantResumeFile"), "UTF-8"));
-                            }
-                            catch (Exception e2){}
-                            /*
-                            applicantResume.getDataInBackground(new GetDataCallback() {
-                                public void done(byte[] data, ParseException e) {
-                                    if (e == null) {
-                                        String doc2 = "a";
-
-                                        try{
-                                            doc2 = new String(data, "UTF-8");
-                                        }
-                                        catch (Exception e2){}
-
-                                        System.out.println(doc2);
-                                    } else {
-                                        // something went wrong
-                                    }
-
-                                }
-                            });
-                            */
-                        }
-
-
-
-                    }
-                    ;
-                } else {
-                }
-            }
-        });
-
-
-
-
     }
 
-    public void uploadData() {
-        Intent intent = getIntent();
-        Bundle temp = intent.getExtras();
-        Uri uri = null;
-        byte[] inputData = null;
-        if (temp != null) {
-            uri = (Uri) temp.get("PATH");
-        }
-        if (uri != null) {
-            try {
-                InputStream iStream = getContentResolver().openInputStream(uri);
-                try {
-                    inputData = getBytes(iStream);
-                    final ParseFile file = new ParseFile(uri.getPath(), "HELLO IS THIS BEING TRANSMITTED".getBytes());
-                    file.saveInBackground(new SaveCallback() {
-                        public void done(ParseException e) {
 
 
-
-                            ParseObject gameScore = new ParseObject("GameScore");
-                            gameScore.put("score", 1337);
-                            gameScore.put("playerName", "Sean Plott");
-                            gameScore.put("playerName22", "Sean Plott");
-                            gameScore.put("cheatMode", false);
-                            gameScore.put("applicantResumeFile", "HELLO IS THIS BEING TRANSMITTED".getBytes());
-                            gameScore.saveInBackground();
-                        }
-                    }, new ProgressCallback() {
-                        public void done(Integer percentDone) {
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");
-                            System.out.println("percentDone");System.out.println("percentDone");
-                            System.out.println("percentDone");
-
-
-                        }
-                    });
-
-
-                } catch (Exception e) {
-                }
-
-            }
-            catch(Exception e){
-                System.out.println("Bug");
-            }
-        }
-    }
-
-    public byte[] getBytes(InputStream inputStream) throws IOException {
-        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
-
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
-        }
-        return byteBuffer.toByteArray();
-    }
-
+    private ArrayAdapter<String> arrayAdapter;
     public void getListFromParse(){
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("TestObject");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Files");
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
                     for (ParseObject x : objects) {
-                        arrayAdapter.add(x.getString("foo"));
+                        arrayAdapter.add(x.getParseFile("File").getName());
                     }
-                    ;
+
                 } else {
                     arrayAdapter.add("Nothing present");
                 }
--- app/src/main/java/com/example/admin/mp3player/UploadActivity.java
+++ app/src/main/java/com/example/admin/mp3player/UploadActivity.java
@@ -1,18 +1,36 @@
 package com.example.admin.mp3player;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ProgressCallback;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.Normalizer;
+
 public class UploadActivity extends AppCompatActivity {
+
+    private ProgressDialog progress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
         showFileChooser();
+
 
     }
 
@@ -41,14 +59,81 @@ public class UploadActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK) {
                     // Get the Uri of the selected file
                     Uri uri = data.getData();
-                    // Get the path
-                    Intent intent = new Intent(this, MP3List.class);
-                    intent.putExtra("PATH",uri);
-                    startActivity(intent);
+                    uploadData(uri);
 
                 }
                 break;
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public synchronized void updateProgress(int percentDone){
+        progress.setProgress(percentDone);
+    }
+
+
+    public void uploadData(Uri uri) {
+
+        /*
+        Progress Dialog Setup
+         */
+        progress = new ProgressDialog(this);
+        progress.setMessage("Uploading to Server...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setCancelable(false);
+        progress.show();
+
+        final Context context = this;
+
+        if (uri != null) {
+            try {
+                InputStream iStream = getContentResolver().openInputStream(uri);
+                try {
+                    final byte[] inputData = getBytes(iStream);
+
+                    /*
+                    NORMALIZE FILE NAME FOR PARSEFILE UPLOAD
+                    */
+                    String temp = uri.getLastPathSegment();
+                    String normalized = Normalizer.normalize(temp, Normalizer.Form.NFD);
+                    String result = normalized.replaceAll("[^A-Za-z0-9]", "");
+                    System.out.println(result);
+
+
+                    final ParseFile file = new ParseFile(result, inputData);
+                    System.out.println(file);
+                    ParseObject fileUpload = new ParseObject("Files");
+                    fileUpload.put("File", file);
+
+                    file.saveInBackground(new SaveCallback() {
+                         @Override
+                         public void done(ParseException e) {
+                             progress.dismiss();
+                             progress = null;
+                             Intent intent = new Intent(context, MP3List.class);
+                             startActivity(intent);
+                         }
+                    });
+
+                    fileUpload.saveInBackground();
+
+                } catch (Exception e) {}
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public byte[] getBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+        return byteBuffer.toByteArray();
     }
 }
